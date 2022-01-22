855daa614aade23acc5897cf3786adef006ec569
Renny Hong <rennyhong1103@gmail.com>
2022-01-22 10:45:22 -0800

    minor improvements

--- firmware/4x6/keymaps/custom/config.h
+++ firmware/4x6/keymaps/custom/config.h
@@ -25,7 +25,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define USE_I2C
 
 /* Select hand configuration */
-//#define MASTER_LEFT
+#define MASTER_LEFT
 //#define MASTER_RIGHT
+
+#define TAPPING_TERM 200
 
 #define EE_HANDS
