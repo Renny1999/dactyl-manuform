7c6b4644c2a92045d776d82513e41e04ff412208
Renny Hong <rennyhong1103@gmail.com>
2022-02-13 14:20:34 -0800

    some changes

--- firmware/4x6/4x6.c
+++ firmware/4x6/4x6.c
@@ -1,2 +1,9 @@
 #include "4x6.h"
 #include "stdint.h"
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        default:
+            return TAPPING_TERM;
+    }
+}
--- firmware/4x6/config.h
+++ firmware/4x6/config.h
@@ -42,3 +42,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_TERM 200
+#define TAPPING_TERM_PER_KEY
+
+
