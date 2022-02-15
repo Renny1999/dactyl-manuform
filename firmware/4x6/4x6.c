38f6382e08a68ea11128e0e491e84a4eedcddcdc
Renny Hong <rennyhong1103@gmail.com>
2022-02-14 23:19:34 -0800

    modified 4x6.c

--- firmware/4x6/4x6.c
+++ firmware/4x6/4x6.c
@@ -7,3 +7,17 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
             return TAPPING_TERM;
     }
 }
+
+bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LT(1, KC_SPACE):
+            // Immediately select the hold action when another key is pressed.
+            return true;
+        case LT(2, KC_SPACE):
+            // Immediately select the hold action when another key is pressed.
+            return true;
+        default:
+            // Do not select the hold action when another key is pressed.
+            return false;
+    }
+}
--- firmware/4x6/config.h
+++ firmware/4x6/config.h
@@ -40,6 +40,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_DI_PIN D3
 #define RGBLED_NUM 12
 
+#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
 #define TAPPING_FORCE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_TERM 200
